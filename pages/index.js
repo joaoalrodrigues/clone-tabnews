b33f97a5e4698d0f8620ea5d729e846747ea57ee
João Alexandre Liberato Rodrigues <jalxnd@gmail.com>
2024-08-04 18:39:04 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Mor, te amo ❤️</h1>
+            <h2>Se você também me ama, dá uma risadinha!</h2>
+        </>
+    )
+}
+
+export default Home;
