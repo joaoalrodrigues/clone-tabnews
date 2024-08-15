b038928138e00ab90a2c5ec33883fa81fa13e5bf
João Alexandre Liberato Rodrigues <jalxnd@gmail.com>
2024-08-15 08:11:55 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(_, response) {
-  response.status(200).json("Oi");
+import database from "../../../../infra/database.js";
+
+async function status(_, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  response.status(200).json({ sum: result.rows.column });
 }
 
 export default status;
